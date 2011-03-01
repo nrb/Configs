5ec6739f193a8581791f8708db8422d21d6b2bfa
Nolan Brubaker <nolan.brubaker@gmail.com>
2011-02-28 19:34:38 -0500

    Added dock reconfiguration.

--- setup-env.sh
+++ setup-env.sh
@@ -6,3 +6,7 @@ pip install virtualenv
 pip install virtualenvwrapper
 
 # install brew, too.
+
+# Move the Dock to the lower right corner.
+default write com.apple.Dock orientation -string right
+default write com.apple.Dock pinning end
