def308e4038a1d1d93dd56c54af953733a82be21
nrb <nolan.brubaker@gmail.com>
2011-05-26 09:07:52 -0400

    Added symbolic link for pip configuration.

--- install-links.sh
+++ install-links.sh
@@ -4,3 +4,4 @@ ln -s ~/Configs/.vim ~/.vim
 ln -s ~/Configs/.vimrc ~/.vimrc
 ln -s ~/Configs/.profile ~/.profile
 ln -s ~/Configs/virtualenvwrapper/postactivate ~/dev/postactivate
+ln -s ~/Configs/pip.conf ~/.pip/pip.conf
