6964cf72de2fac25a181525e236b08eb686c3cc6
Nolan Brubaker <nolan@nbrubaker.com>
2012-01-26 16:56:45 -0500

    Add vim bindings for readline and editline libs

--- install-links.sh
+++ install-links.sh
@@ -5,3 +5,5 @@ ln -s ~/Configs/.vimrc ~/.vimrc
 ln -s ~/Configs/.profile ~/.profile
 ln -s ~/Configs/virtualenvwrapper/postactivate ~/dev/postactivate
 ln -s ~/Configs/pip.conf ~/.pip/pip.conf
+ln -s ~/Configs/.editrc ~/.editrc
+ln -s ~/Configs/.inputrc ~/.inputrc
